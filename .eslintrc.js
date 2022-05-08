fa6dfe96addd41b08193cd9e3afe3c56f38ae6b4
glows777 <1914426389@qq.com>
2022-05-08 13:39:56 +0800

    feat: eslint&prettier

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ['plugin:vue/strongly-recommended', 'plugin:prettier/recommended'],
+  // add your custom rules here
+  rules: {
+    // ...other codes
+    'prettier/prettier': 'error',
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/html-closing-bracket-spacing': 'off',
+    'vue/html-self-closing': 'off',
+    'vue/html-indent': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/require-prop-types': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/no-template-shadow': 'off'
+  }
+};
